9ebd644fba7e4ae552d731cfb84e5b31b026c3d8
default <george.dewey2006@icloud.com>
2026-03-29 11:47:09 +0000

    fixed titles

--- pages/admin/course-administration.php
+++ pages/admin/course-administration.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Course Administration</title>
     
     <!-- Bootstrap CSS for responsive layout and components -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
--- pages/user/explore-courses.php
+++ pages/user/explore-courses.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Karting Pro - Explore Courses</title>
+    <title>Explore Courses</title>
     
     <!-- Bootstrap and Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
