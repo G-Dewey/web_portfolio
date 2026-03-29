440af34572a576356b43f0c61747e605912300bf
default <george.dewey2006@icloud.com>
2026-03-29 00:56:26 +0000

    fix delete user issue

--- api/users/delete-user.php
+++ api/users/delete-user.php
@@ -7,6 +7,8 @@ if (!isset($_POST['userID'])){
 }
 
 require_once("pages/_connect.php");
+require_once("api/enrollments/enrollment-functions.php");
+
 $userID = $_POST['userID'];
 
 if ($userID == $_SESSION['userID']){
@@ -22,14 +24,30 @@ mysqli_stmt_execute($stmt);
 
 // Removes all enrollments tied to that user 
 try{
-    $SQL = "DELETE from `enrollments`
-            WHERE `enrollments`.`userID` = ?";
 
-    $stmt = mysqli_prepare($connect, $SQL);
-    
-    mysqli_stmt_bind_param($stmt, "i", $userID);
-    mysqli_stmt_execute($stmt);
+    // Gets the courseID before deleting enrollments 
+    $enrollSQL = "SELECT DISTINCT courseID FROM enrollments WHERE userID = ?;";
+    $enrollStmt = mysqli_prepare($connect, $enrollSQL);
+    mysqli_stmt_bind_param($enrollStmt, "i", $userID);
+    mysqli_stmt_execute($enrollStmt);
+    $enrollResult = mysqli_stmt_get_result($enrollStmt);
 
+    // Store the courseIDs in an array
+    $courseIDs = [];
+    while ($row = mysqli_fetch_assoc($enrollResult)) {
+        $courseIDs[] = $row['courseID'];
+    }
+
+    // Delete all enrollments for this user
+    $deleteEnrollSQL = "DELETE FROM enrollments WHERE userID = ?;";
+    $deleteEnrollStmt = mysqli_prepare($connect, $deleteEnrollSQL);
+    mysqli_stmt_bind_param($deleteEnrollStmt, "i", $userID);
+    mysqli_stmt_execute($deleteEnrollStmt);
+
+    // Update enrolled count for each affected course
+    foreach ($courseIDs as $courseID) {
+        updateEnrolledCount($connect, $courseID);
+    }
 } catch (Exception $e) {
     // No error code as it has still be deleted
     die();
